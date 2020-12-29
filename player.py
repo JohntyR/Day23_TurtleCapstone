3d84e1a330e8fbefe85aa944d05674b533a90e56
John Ronis <jronis@gmail.com>
2020-12-29 15:17:42 +1000

    player movement

--- player.py
+++ player.py
@@ -1,7 +1,17 @@
+from turtle import Turtle
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
+UP = 90
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.speed("fastest")
+        self.setheading(UP)
+
+    def up(self):
+        self.forward(MOVE_DISTANCE)
