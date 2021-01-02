273b492f6664f4c67558cbe367a6b8bca073ca9a
John Ronis <jronis@gmail.com>
2021-01-02 13:05:24 +1000

    implemented scoreboard, refactored levels into scoreboard, gameover sequence

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,34 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = "Tahoma"
+FONT_SIZE = 16
+FONT_WEIGHT = "normal"
+FONT_COLOUR = "black"
+X_POS = -250
+Y_POS = 270
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 0
+        self.color(FONT_COLOUR)
+        self.penup()
+        self.goto(x=X_POS, y=Y_POS)
+        self.hideturtle()
+        self.refresh()
+
+    def increment_level(self):
+        self.level += 1
+        self.refresh()
+
+    def refresh(self):
+        self.clear()
+        self.write(
+            f"Level: {self.level}", align=ALIGNMENT, font=(FONT, FONT_SIZE, FONT_WEIGHT)
+        )
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align=ALIGNMENT, font=(FONT, FONT_SIZE, FONT_WEIGHT))
